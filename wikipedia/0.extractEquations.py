a9e150ed3a9648f8b289f808b72fc661e1f348a3
Vanessa Sochat <vsochat@stanford.edu>
2018-11-21 19:31:28 -0500

    cleaning up parsing of equations

--- wikipedia/0.extractEquations.py
+++ wikipedia/0.extractEquations.py
@@ -1,62 +1,90 @@
 from repofish.utils import ( save_json, save_txt, convert_unicode )
 from repofish.wikipedia import get_page
+from wikipedia import WikipediaPage
 from exceptions import KeyError
-import pandas
-import numpy
-import time
 
-## STEP 1: PARSE METHOD TEXT ###################################################
+## STEP 1: METHODS #############################################################
+#  Get a list of (disambiguated) methods from Wikipedia
 
 methods_url = "https://en.wikipedia.org/wiki/List_of_statistics_articles"
 topic = "List_of_statistics_articles"
-result = get_page(topic)
+result = WikipediaPage(topic)
 
 # Save list of methods in case it changes
-methods = pandas.DataFrame()
-methods["methods"] = result.links
+methods = set(result.links)
 
-# This is manual parsing to resolve disambiguation errors - we will have redundancy here, 
-# but not an issue as results object is a dictionary
-methods[methods.methods=="A posteriori probability (disambiguation)"] = "posterior probability"
-methods[methods.methods=="ANCOVA"] = "Analysis of covariance" # ANCOVA would return "Angola"
-methods[methods.methods=="Data generating process"] = "data collection"
-methods[methods.methods=="Data generating process (disambiguation)"] = "data collection"
-methods[methods.methods=="Deviation analysis"] = "absolute difference"
-methods[methods.methods=="Deviation analysis (disambiguation)"] = "absolute difference"
-methods[methods.methods=="Double exponential distribution"] = "Laplace distribution"
-methods[methods.methods=="Double exponential distribution (disambiguation)"] = "Laplace distribution"
-methods[methods.methods=="Lambda distribution (disambiguation)"] = "Tukey's lambda distribution"
-methods[methods.methods=="Lambda distribution"] = "Tukey's lambda distribution"
-methods[methods.methods=="Linear least squares"] = "Linear least squares (mathematics)"
-methods[methods.methods=="Linear least squares (disambiguation)"] = "Linear least squares (mathematics)"
-methods[methods.methods=="MANCOVA"] = "Multivariate analysis of covariance"
-methods[methods.methods=="MANCOVA (disambiguation)"] = "Multivariate analysis of covariance"
-methods[methods.methods=="Mean deviation"] = "Mean signed deviation"
-methods[methods.methods=="Mean deviation (disambiguation)"] = "Mean signed deviation"
-methods[methods.methods=="Safety in numbers (disambiguation)"] = "Safety in numbers"
-methods[methods.methods=="Safety in numbers"] = "Safety in numbers"
-methods[methods.methods=="T distribution"] = "Student's t-distribution"
-methods[methods.methods=="T distribution (disambiguation)"] = "Student's t-distribution"
-methods[methods.methods=="Analyse-it"] = 'Analyze (imaging software)'
 
-methods.to_csv("wikipedia_methods.tsv",sep="\t",encoding="utf-8")
+def update_method_name(methods, old_name, new_name):
+    '''update the set by removing an old name (usually a disambuguation error)
+       with a new name
+
+       Parameters
+       ==========
+       methods: the set of methods
+       oldName: the name to remove
+       newName: the name to add
+    '''
+    if old_name in methods:
+        methods.remove(old_name)
+    methods.add(new_name)
+    return methods
+
+# These tuples are in the format (<old_name> , <new_name>)
+
+disambiguations = (
+    ("A posteriori probability (disambiguation)","posterior probability"),
+    ("Data generating process", "data collection"),
+    ("Data generating process (disambiguation)", "data collection"),
+    ("Deviation analysis", "absolute difference"),
+    ("Deviation analysis (disambiguation)", "absolute difference"),
+    ("Double exponential distribution", "Laplace distribution"),
+    ("Double exponential distribution (disambiguation)", "Laplace distribution"),
+    ("Lambda distribution (disambiguation)", "Tukey's lambda distribution"),
+    ("Lambda distribution", "Tukey's lambda distribution"),
+    ("MANCOVA (disambiguation)", "Multivariate analysis of covariance"),
+    ("Mean deviation", "Mean signed deviation"),
+    ("Mean deviation (disambiguation)", "Mean signed deviation"),
+    ("Safety in numbers (disambiguation)", "Safety in numbers"),
+    ("T distribution", "Student's t-distribution"),
+    ("T distribution (disambiguation)", "Student's t-distribution"),
+    ("Contiguity", "Contiguous distribution"),
+    ("Correctness", "Correctness (computer science)"),
+    ("Energy statistics","E-statistics"),
+    ("Sum of squares", "Sum of squares function"),
+    ("Sum of squares (disambiguation)", "Sum of squares function")
+)
+
+
+# This is manual parsing to resolve disambiguation errors, 
+# we will have redundancy here but not an issue as results is a dictionary
+for pair in disambiguations:
+    methods = update_method_name(methods, pair[0], pair[1])
+
+removals = ['MetaNSUE', 'Player wins']
+
+# in the list, but wikipedia doesn't have defined
+for removal in removals:
+    methods.remove(remove)
+
+# Save list to file, wikipedia changes!
+save_txt('\n'.join(list(methods)), "wikipedia_methods.txt")
 
 # Now let's generate a data structure with all content, links, etc.
 results = dict()
 
 def get_attribute(entry, name):
+    '''in the case that a result object doesn't links or references, it would
+       throw an error. So check first.'''
     try:
         if hasattr(result, name):
-            return getattr(result, name)
-
-    # no links :)
+            return getattr(result, name)    
     except KeyError:
         pass
     return []
 
-for method in methods["methods"]:
+for method in methods:
     if method not in results:
-        result = get_page(method)
+        result = WikipediaPage(method)
 
         # Show a visual check!
         print("Matching %s to %s" %(method,result.title))
